5744d4844a8887d4793349dbf3057b3dbdbc0d7f
eag1ex <eaglex@eaglex.net>
2021-02-23 11:26:12 +0000

    reduce material assets and minor css tweaks

--- libs/material/src/lib/material.module.ts
+++ libs/material/src/lib/material.module.ts
@@ -5,25 +5,25 @@
 import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
-import { MatProgressBarModule } from '@angular/material/progress-bar';
-import { MatCardModule } from '@angular/material/card';
+// import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+// import { MatProgressBarModule } from '@angular/material/progress-bar';
+// import { MatCardModule } from '@angular/material/card';
 import { MatChipsModule } from '@angular/material/chips';
 import { MatDividerModule } from '@angular/material/divider';
 import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatMenuModule } from '@angular/material/menu';
+// import { MatMenuModule } from '@angular/material/menu';
 import { MatSelectModule } from '@angular/material/select';
 
 const ANGULAR_MATERIAL_MODULES = [
   MatToolbarModule,
   MatSelectModule,
   MatIconModule,
-  MatMenuModule,
+ // MatMenuModule,
   MatButtonModule,
-  MatCardModule,
+ // MatCardModule,
   MatChipsModule,
-  MatProgressSpinnerModule,
-  MatProgressBarModule,
+  // MatProgressSpinnerModule,
+  // MatProgressBarModule,
   MatDividerModule,
 ];
 
